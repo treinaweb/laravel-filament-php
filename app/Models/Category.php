96dc801a44ce7b40ec8c533df778ba7bfafdbc97
Elton Fonseca <elton869@gmail.com>
2024-10-31 15:44:59 -0300

    15 - Configurando o relacionamento

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -3,8 +3,17 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Category extends Model
 {
-    //
+    /**
+     * Uma categoria possui muitos produtos
+     * 
+     * @return HasMany 
+     */
+    public function products(): HasMany
+    {
+        return $this->hasMany(Product::class);
+    }
 }
--- app/Models/Product.php
+++ app/Models/Product.php
@@ -3,8 +3,19 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Product extends Model
 {
     protected $fillable = ['name', 'slug', 'description'];
+
+    /**
+     * Um produto pertence a uma categoria
+     * 
+     * @return BelongsTo 
+     */
+    public function category(): BelongsTo
+    {
+        return $this->belongsTo(Category::class);
+    }
 }
